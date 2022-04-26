16e165595fa72d4fe12dbc211d36e229fbc8d20d
m43 <frano.rajich@gmail.com>
2022-04-26 11:54:39 +0200

    Add 512x512 decoder support

--- svglatte/models/neural_rasterizer.py
+++ svglatte/models/neural_rasterizer.py
@@ -268,7 +268,6 @@ def get_dataset(config):
             clean_disk_cache=config.clean_disk_cache,
         )
     elif config.dataset == "argoverse":
-        seq_feature_dim = argoverse_dataset.SEQ_FEATURE_DIM
         dm = argoverse_dataset.ArgoverseDataModule(
             data_root=config.argoverse_data_root,
             rendered_images_width=config.argoverse_rendered_images_width,
@@ -364,11 +363,11 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
         decoder = []
         assert len(kernel_size_list) == len(stride_list) == len(padding_list) == len(output_padding_list)
-        image_sizes = []
+        self.image_sizes = []
         image_size = 1
         for k, s, p, op in zip(kernel_size_list, stride_list, padding_list, output_padding_list):
             image_size = (image_size - 1) * s - 2 * p + 1 * (k - 1) + op + 1
-            image_sizes.append(image_size)
+            self.image_sizes.append(image_size)
         n_upsampling = len(kernel_size_list)
         mult = 2 ** n_upsampling
 
@@ -380,7 +379,9 @@ class Decoder(nn.Module):
             padding=padding_list[0],
             output_padding=output_padding_list[0]
         )
-        decoder += [conv, norm_layer([int(ngf * mult / 2), image_sizes[0], image_sizes[0]]), nn.ReLU(True)]
+        decoder += [conv,
+                    norm_layer([int(ngf * mult / 2), self.image_sizes[0], self.image_sizes[0]]),
+                    nn.ReLU(True)]
         for i in range(1, n_upsampling):
             mult = 2 ** (n_upsampling - i)
             conv = nn.ConvTranspose2d(
@@ -391,7 +392,9 @@ class Decoder(nn.Module):
                 padding=padding_list[i],
                 output_padding=output_padding_list[i]
             )
-            decoder += [conv, norm_layer([int(ngf * mult / 2), image_sizes[i], image_sizes[i]]), nn.ReLU(True)]
+            decoder += [conv,
+                        norm_layer([int(ngf * mult / 2), self.image_sizes[i], self.image_sizes[i]]),
+                        nn.ReLU(True)]
         decoder += [nn.Conv2d(ngf, out_channels, kernel_size=7, padding=7 // 2)]
         decoder += [nn.Sigmoid()]
         self.decode = nn.Sequential(*decoder)
@@ -423,9 +426,22 @@ def main(config):
             ngf=4,  # 64
             # norm_layer=nn.LayerNorm,
         )
+    elif config.argoverse_rendered_images_width == 512 and config.argoverse_rendered_images_height == 512:
+        decoder = Decoder(
+            in_channels=encoder.output_size,
+            out_channels=1,
+            kernel_size_list=(3, 3, 3, 3, 3, 5, 5, 5, 5),
+            stride_list=(2, 2, 2, 2, 2, 2, 2, 2, 2),
+            padding_list=(1, 1, 1, 1, 1, 2, 2, 2, 2),
+            output_padding_list=(1, 1, 1, 1, 1, 1, 1, 1, 1),
+            ngf=4,  # 64
+            # norm_layer=nn.LayerNorm,
+        )
     else:
         raise Exception(f"Image size {config.argoverse_rendered_images_width}x{config.argoverse_rendered_images_height}"
                         f" not supported")
+    assert config.argoverse_rendered_images_width == decoder.image_sizes[-1]
+    assert config.argoverse_rendered_images_height == decoder.image_sizes[-1]
 
     adam_optimizer_args = AttrDict()
     adam_optimizer_args.update({
