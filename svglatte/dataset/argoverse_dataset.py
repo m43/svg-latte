b8c3f327caf896a4933fd3e75415617796a66c4d
m43 <frano.rajich@gmail.com>
2022-04-30 20:33:28 +0200

    Verify that the unprocessed sequences indeed are in the (0,0) -- (24,24) viewbox.

--- svglatte/dataset/argoverse_dataset.py
+++ svglatte/dataset/argoverse_dataset.py
@@ -311,7 +311,7 @@ class ArgoverseDataModule(pl.LightningDataModule):
         )
 
 
-if __name__ == '__main__':
+def main1():
     print("load datasets")
     val_ds_1 = ArgoverseDataset(
         caching_path_prefix="/home/user72/Desktop/argoverse1/val",
@@ -347,3 +347,35 @@ if __name__ == '__main__':
     plt.close()
 
     print("done")
+
+
+def main2():
+    for ds_name in ["val", "test", "train"]:  # val is the smallest, train the largest subset.
+        print(f"Loading subset: {ds_name}")
+        ds = ArgoverseDataset(
+            caching_path_prefix=f"/home/user72/Desktop/argoverse1/{ds_name}",
+            rendered_images_width=64,
+            rendered_images_height=64,
+            render_on_the_fly=True,
+            remove_redundant_features=True,
+            numericalize=False,
+            augment=False,
+        )
+        print(f"Loaded the subset: {ds_name}")
+        print(f"Number of datapoints: {len(ds._sequences)}")
+
+        def set_unused_to_some_constant(seq, const=5):
+            # I put five because I know that my data has a veiwbox of 24, so the xy coordinates
+            # are all in the box (0,0)-(24,24).
+            return (seq != -1) * seq + (seq == -1) * const
+
+        print(torch.cat([set_unused_to_some_constant(seq).max(0).values[None, :] for seq in ds._sequences]).max(0))
+        print(torch.cat([set_unused_to_some_constant(seq).min(0).values[None, :] for seq in ds._sequences]).min(0))
+
+        print(f"Done with {ds}")
+        print()
+        print()
+
+
+if __name__ == '__main__':
+    main2()
